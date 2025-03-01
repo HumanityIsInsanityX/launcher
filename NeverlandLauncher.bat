ebe77ce13259e0d45fb1915c420d8c81f20f6179
Humanity <112782907+HumanityIsInsanityX@users.noreply.github.com>
2025-03-01 10:58:34 -0600

    Create NeverlandLauncher.bat

---
+++ NeverlandLauncher.bat
@@ -0,0 +1,6 @@
+@echo off
+for /f "delims=" %%F in ('where /r C:\ DayZ_BE.exe 2^>nul') do (
+    start "" "%%F" ^
+        "-mod=!Workshop\@Stargate Teleporters Complete;!Workshop\@CF;!Workshop\@Deadfall;!Workshop\@Dabs Framework;!Workshop\@DayZ Editor Loader;!Workshop\@VPPAdminTools;!Workshop\@Ancient_ruin;!Workshop\@ModelsUK;!Workshop\@Custom Keycards;!Workshop\@Survivor Animations;!Workshop\@DayZ Horse;!Workshop\@Bushcraft;!Workshop\@NeverlandRP"
+    exit /b
+)
